517ed0fcdda2f099a16dfa85ebf296696e296092
Costbound <popovskiy96@gmail.com>
2024-06-28 06:50:07 +0000

    Revert "eneable create contact join validation, disable patch"
    This reverts commit 11d213318cf89fbd5be75e50ce02767204acd593.
    
    Revert auto login after registration

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -31,7 +31,7 @@ router.get(
 router.post(
   '/',
   upload.single('photo'),
-  validateBody(createContactSchema),
+  // validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
